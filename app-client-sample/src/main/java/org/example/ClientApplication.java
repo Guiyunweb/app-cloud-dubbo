b69ca01d7c8a07710aa66c3612128d4df15eb070
Guiyun <ryougicloud@gmail.com>
2021-11-11 17:25:18 +0800

    消费者完成

---
+++ app-client-sample/src/main/java/org/example/ClientApplication.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by myy on 2021/11/11.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
---
+++ app-client-sample/src/main/java/org/example/controller/EchoController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.example.service.EchoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by myy on 2021/11/11.
+ */
+@RestController
+public class EchoController {
+
+    @DubboReference
+    private EchoService echoService;
+
+    @GetMapping("/echo")
+    public String echo(String message) {
+        return echoService.echo(message);
+    }
+
+}
