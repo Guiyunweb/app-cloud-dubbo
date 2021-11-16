cae926f8f921c6260ac72b96cabd07dbfcdef336
Guiyun <ryougicloud@gmail.com>
2021-11-16 19:10:37 +0800

    添加docker

--- app-common/src/main/java/org/example/common/utils/GsonUtil.java
+++ app-common/src/main/java/org/example/common/utils/GsonUtil.java
@@ -17,9 +17,7 @@ public class GsonUtil {
 
     private static Gson gson = null;
     static {
-        if (gson == null) {
-            gson = new Gson();
-        }
+        gson = new Gson();
     }
 
 
@@ -28,12 +26,7 @@ public class GsonUtil {
 
 
 
-    /**
-     * 将object对象转成json字符串
-     *
-     * @param object
-     * @return
-     */
+
     public static String GsonToString(Object object) {
         String gsonString = null;
         if (gson != null) {
@@ -43,13 +36,6 @@ public class GsonUtil {
     }
 
 
-    /**
-     * 将gsonString转成泛型bean
-     *
-     * @param gsonString
-     * @param cls
-     * @return
-     */
     public static <T> T GsonToBean(String gsonString, Class<T> cls) {
         T t = null;
         if (gson != null) {
@@ -59,13 +45,7 @@ public class GsonUtil {
     }
 
 
-    /**
-     * 转成list
-     * 泛型在编译期类型被擦除导致报错
-     * @param gsonString
-     * @param cls
-     * @return
-     */
+
     public static <T> List<T> GsonToList(String gsonString, Class<T> cls) {
         List<T> list = null;
         if (gson != null) {
@@ -76,14 +56,6 @@ public class GsonUtil {
     }
 
 
-    /**
-     * 转成list
-     * 解决泛型问题
-     * @param json
-     * @param cls
-     * @param <T>
-     * @return
-     */
     public static <T> List<T> JsonToList(String json, Class<T> cls) {
         Gson gson = new Gson();
         List<T> list = new ArrayList<T>();
@@ -97,12 +69,7 @@ public class GsonUtil {
 
 
 
-    /**
-     * 转成list中有map的
-     *
-     * @param gsonString
-     * @return
-     */
+
     public static <T> List<Map<String, T>> GsonToListMaps(String gsonString) {
         List<Map<String, T>> list = null;
         if (gson != null) {
@@ -114,12 +81,7 @@ public class GsonUtil {
     }
 
 
-    /**
-     * 转成map的
-     *
-     * @param gsonString
-     * @return
-     */
+
     public static <T> Map<String, T> GsonToMaps(String gsonString) {
         Map<String, T> map = null;
         if (gson != null) {
@@ -129,11 +91,7 @@ public class GsonUtil {
         return map;
     }
 
-    /**
-     * 把一个bean（或者其他的字符串什么的）转成json
-     * @param object
-     * @return
-     */
+
     public static String BeanToJson(Object object){
         return gson.toJson(object);
     }
